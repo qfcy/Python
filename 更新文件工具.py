c496cf8b0b8b33a67da9ab87296570d67c0ee00c
qfcy <3416445406@qq.com>
2022-02-02 22:46:14 +0800

    修改 更新文件工具

--- 更新文件工具.py
+++ 更新文件工具.py
@@ -1,4 +1,6 @@
 # 程序功能: 更新目标目录中的文件, 进行数据备份
+# 用途: 包含大量文件的文件夹的备份, 解决备份文件夹时
+# 需要复制整个文件夹的问题
 import sys,os,shutil
 from search_file import direc
 
@@ -7,16 +9,22 @@ def normpath(path):
     if path.endswith(':'):
         path += '\\'
     return path
-    
-src = normpath(input('输入源目录: ')).strip('"').strip()
-dst = normpath(input('输入目标目录: ')).strip('"').strip()
+
+if len(sys.argv) == 3:
+    src,dst = sys.argv[1:]
+else:
+    print('用法:%s <源目录> <目标目录>'%sys.argv[0])
+    src = normpath(input('输入源目录: ')).strip('"').strip()
+    dst = normpath(input('输入目标目录: ')).strip('"').strip()
+
 def copy2(src,dst):
     if not os.path.isdir(os.path.split(dst)[0]):
         os.makedirs(os.path.split(dst)[0],exist_ok=True)
     shutil.copy2(src,dst)
 
+all_=False;ignore_all=False
 for file in direc(src,dirs=False):
-    dst_file = dst + file.replace(src,'')
+    dst_file = dst + file[len(src):] # 原为file.replace(src,'')
     if os.path.isfile(dst_file):
         # 用源目录中新的文件替换旧的文件
         if os.stat(file).st_mtime > os.stat(dst_file).st_mtime:
@@ -24,28 +32,50 @@ for file in direc(src,dirs=False):
             copy2(file,dst_file)
         elif os.stat(file).st_mtime < os.stat(dst_file).st_mtime:
             # 目标目录中文件较新时
-            ans=input('是否复制 %s 到 %s ? (Y/N)' % (dst_file,file))
-            if ans.lower().startswith('y'):
+            
+            if all_:
                 copy2(dst_file,file)
+            elif not ignore_all:
+                ans=input('是否复制 %s 到 %s ? [Y/N/A(All)/I(Ignore all)]'\
+                          % (dst_file,file))
+                if ans.lower().startswith('y'):
+                    copy2(dst_file,file)
+                elif ans.lower() in ('a','all'):
+                    all=True;copy2(dst_file,file)
+                elif ans.lower() in ('i','ignore all'):
+                    ignore_all=True
+            else:
+                print('忽略 %s'%dst_file)
     else:
         # 旧文件不存在时, 直接复制
         print('已复制:',file,dst_file)
         copy2(file,dst_file)
 
 # 删除目标目录中存在, 而源目录相同位置不存在的文件
-all=False
+all_=False;ignore_all=False
 for file in direc(dst,dirs=False):
     if not os.path.isfile(
-        os.path.join(src, file.replace(dst,'').lstrip('\\'))):
-        if all:
+        os.path.join(src, file[len(dst):].lstrip('\\'))):
+        if all_:
             print('已删除 '+file)
             os.remove(file)
-        else:
-            ans=input('删除 %s ? (Y/N/All)' % (file))
+        elif not ignore_all:
+            ans=input('删除 %s ? [Y/N/A(All)/I(Ignore all)]' % (file))
             if ans.lower().startswith('y'):
                 os.remove(file)
-            elif ans.lower() == 'all':
-                all=True;os.remove(file)
+            elif ans.lower() in ('a','all'):
+                all_=True;os.remove(file)
+            elif ans.lower() in ('i','ignore all'):
+                    ignore_all=True
+        else:
+            print('忽略 %s'%file)
+
+# 删除目标目录中存在, 而源目录不存在的空目录
+for dir_ in direc(dst,files=False):
+    if not os.listdir(dir_)\
+and not os.path.isfile(os.path.join(src, dir_[len(dst):].lstrip('\\'))):
+        os.removedirs(dir_)
+        print('已删除空目录 %s'%dir_)
 
 if not 'pythonw' in os.path.split(sys.executable)[1]:
     os.system('pause')
