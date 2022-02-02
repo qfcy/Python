f7adfa10a2fc01b3c7114b583c8d5756a15eb2e0
qfcy <3416445406@qq.com>
2022-02-02 15:27:04 +0800

    Update 文件修改日期.py

--- 算法/文件修改日期.py
+++ 算法/文件修改日期.py
@@ -1,4 +1,4 @@
 import sys,os,time
 for arg in sys.argv[1:]:
     os.utime(arg,
-             (time.mktime(time.strptime("2108 01 07:59:58", "%Y %d %H:%M:%S")),)*2)
+             (time.mktime(time.strptime("2108 01 01 07:59:58", "%Y %m %d %H:%M:%S")),)*2)
