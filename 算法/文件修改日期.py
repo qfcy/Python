cbea28e1076bf3f46ec7c328337514424d9e48e5
qfcy <3416445406@qq.com>
2022-02-05 18:29:20 +0800

    Update 文件修改日期.py

--- 算法/文件修改日期.py
+++ 算法/文件修改日期.py
@@ -1,4 +1,7 @@
 import sys,os,time
+# 时间最高支持 2108 01 01 (07:59:58)
+format = "%Y %m %d"
+time_ = input('输入 %s 格式的时间: '%format)
 for arg in sys.argv[1:]:
     os.utime(arg,
-             (time.mktime(time.strptime("2108 01 01 07:59:58", "%Y %m %d %H:%M:%S")),)*2)
+             (time.mktime(time.strptime(time_, format)),)*2)
