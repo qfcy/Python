2d1f0e7034106757ced4b5e7be1452c3725be565
qfcy <3416445406@qq.com>
2022-03-19 22:09:41 +0800

    Delete 源码 directory

--- 源码/listing_8-5.py
+++
@@ -1,9 +0,0 @@
-# Listing_8-5.py
-# Copyright Warren & Carter Sande, 2013
-# Released under MIT license   http://www.opensource.org/licenses/mit-license.php
-# Version $version  ----------------------------
-
-# Printing the 8 times table using range()
-
-for looper in range(1, 11):
-    print looper, "times 8 =", looper * 8
