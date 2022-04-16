76663729cfee438c702cb6786b6e88d1bb3d2d29
qfcy <3416445406@qq.com>
2022-04-16 15:30:53 +0800

    Other changes

---
+++ PyGlet/opengl.py
@@ -0,0 +1,36 @@
+import numpy as np
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+def init_condition():
+    glClearColor(1.0, 1.0, 1.0, 1.0)  # 定义背景为白色
+    gluOrtho2D(-8.0, 8.0, -8.0, 8.0)  # 定义xy轴范围
+def render():
+    pass
+def draw_geometry():
+    glClear(GL_COLOR_BUFFER_BIT)
+    glColor3f(0, 1.0, 1.0)  # 设定颜色RGB
+    glBegin(GL_POLYGON)
+    #glVertex2f(-2, 2)
+    #glVertex2f(-2, 5)
+    #glVertex2f(-5, 5)
+    #glVertex2f(-5, 2)
+
+    glColor3f(1,0,0)
+    glVertex3f(-5,-5,0.0)
+    glColor3f(0, 5, 0)
+    glVertex3f(1, -5, 0)
+    glColor3f(0, 0, 5)
+    glVertex3f(0, 5, 0)
+
+    glEnd()
+    glFlush()  # 执行绘图
+
+glutInit()  # 启动glut
+glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA)
+glutInitWindowSize(400, 400)
+glutCreateWindow(b"Hello OpenGL")  # 设定窗口标题
+glutDisplayFunc(draw_geometry)  # 调用函数绘制
+init_condition()  # 设定背景
+glutMainLoop()
--- turtle/shapes/pdf.py
+++ turtle/shapes/pdf.py
@@ -1,13 +1,13 @@
 from reportlab.graphics.shapes import Drawing,PolyLine
 from reportlab.graphics import renderPDF
 from turtle import *
-import 房子
+import house
 
 delay(0)
 penup()
 goto(400,300)
 begin_poly()
-房子.draw(60)
+house.draw(60)
 end_poly()
 d=Drawing(window_width(),window_height())
 s=get_poly()
