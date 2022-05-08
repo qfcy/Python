d269630e6eca4548da0b44eff3c427504f9d7b5b
qfcy <3416445406@qq.com>
2022-05-08 11:50:18 +0800

    Update Windows  搜索打开测试.py

--- Windows  搜索打开测试.py
+++ Windows  搜索打开测试.py
@@ -1,8 +1,8 @@
-import subprocess
+import os
 from urllib.parse import quote
 kw = input("输入搜索词: ")
 path = input("输入路径: ")
-url = 'search-ms:displayname=Python&crumb=&crumb=System.Generic.String:%s&crumb=location:%s'%(quote(kw),quote(path))
+url = 'search-ms:displayname=Python&crumb=&crumb=System.Generic.String:%s&crumb=location:%s,'%(quote(kw),quote(path))
 print(url)
-subprocess.run(["explorer.exe",'/root,',url])
-input()
+# ^将&符号转义
+os.system('start '+url.replace('&','^&'))
