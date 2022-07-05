2f8631d23e9cb4da991f69acaf31e5730728f934
qfcy <3416445406@qq.com>
2022-07-05 11:21:54 +0800

    Update Windows  搜索框模拟.py

--- Windows  搜索框模拟.py
+++ Windows  搜索框模拟.py
@@ -1,11 +1,25 @@
 import os
-from urllib.parse import quote
+from urllib.parse import quote_from_bytes as _quote
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter.font import Font
-from tk_dragtool import draggable
+from tk_dragtool import bind_drag
 import tkinter.messagebox as msgbox
 import windnd
+
+_safe = '''ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+abcdefghijklmnopqrstuvwxyz\
+0123456789_.-~'''
+# 重写quote方法, 使支持中文
+# 已知: 系统不能识别已被quote的中文
+def quote(string):
+    result = ''
+    for c in string:
+        if ord(c) < 128 and c not in _safe:
+            result+=_quote(c.encode())
+        else:
+            result+=c
+    return result
 
 path=None
 def ondrag(filenames):
@@ -19,8 +33,9 @@ def ondrag(filenames):
 def launch_search():
     if path is None:
         msgbox.showinfo("","您还没有选择搜索范围!")
+        return
     word = kw.get()
-    title = path # bug:路径为中文时有乱码
+    title = path
     url = 'search-ms:displayname=%s&crumb=&crumb=System.Generic.String:%s&crumb=location:%s'%(
         quote(title), quote(word), quote(path))
     print(url)
@@ -31,12 +46,15 @@ root=tk.Tk()
 root.title("Windows 搜索框模拟")
 root.geometry("400x60")
 #root.overrideredirect(True) 去除窗口的边框
-f=Font(root,size=20)
 tip=tk.Label(root,text="拖曳搜索范围的文件夹到此: ")
 tip.pack(side=tk.TOP,fill=tk.X)
+bind_drag(root,tip)
+
 kw=ttk.Entry(root,font=("Microsoft Yahei",12,"normal"))
 kw.pack(side=tk.LEFT,expand=True,fill=tk.BOTH)
+
 btn=ttk.Button(root,text="搜索",command=launch_search,width=6)
 btn.pack(side=tk.RIGHT,fill=tk.Y)
+
 windnd.hook_dropfiles(root,func=ondrag)
 root.mainloop()
