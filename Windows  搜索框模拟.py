17823c14fe63ae252930c8052c439842c22a5c95
qfcy <3416445406@qq.com>
2022-07-07 22:24:39 +0800

    Update Windows  搜索框模拟.py

--- Windows  搜索框模拟.py
+++ Windows  搜索框模拟.py
@@ -14,7 +14,7 @@ abcdefghijklmnopqrstuvwxyz\
 # 已知: 系统不能识别已被quote的中文
 def quote(string):
     result = ''
-    for c in string:
+    for c in string:# _safe中的字符, 和非ASCII字符可以保留在url中
         if ord(c) < 128 and c not in _safe:
             result+=_quote(c.encode())
         else:
@@ -48,7 +48,7 @@ root.geometry("400x60")
 #root.overrideredirect(True) 去除窗口的边框
 tip=tk.Label(root,text="拖曳搜索范围的文件夹到此: ")
 tip.pack(side=tk.TOP,fill=tk.X)
-bind_drag(root,tip)
+bind_drag(root,tip) # 使用tk-dragtool库绑定拖曳
 
 kw=ttk.Entry(root,font=("Microsoft Yahei",12,"normal"))
 kw.pack(side=tk.LEFT,expand=True,fill=tk.BOTH)
@@ -56,5 +56,5 @@ kw.pack(side=tk.LEFT,expand=True,fill=tk.BOTH)
 btn=ttk.Button(root,text="搜索",command=launch_search,width=6)
 btn.pack(side=tk.RIGHT,fill=tk.Y)
 
-windnd.hook_dropfiles(root,func=ondrag)
+windnd.hook_dropfiles(root,func=ondrag) # 绑定文件夹拖入事件
 root.mainloop()
