fa6077f4416c01bb1786255ab094b86d840c1c94
qfcy <3416445406@qq.com>
2022-07-12 17:24:26 +0800

    Updated event 1.1.1

--- event/__init__.py
+++ event/__init__.py
@@ -2,6 +2,6 @@
 A Python package simulating keyboard and mouse events ,which can be used in automation programs."""
 __email__="3416445406@qq.com"
 __author__="七分诚意 qq:3076711200 邮箱:%s"%__email__
-__version__="1.1.0"
+__version__="1.1.1"
 import event.key as key
 import event.mouse as mouse
