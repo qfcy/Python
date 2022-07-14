e2b8a88834d61b68dc496ab0ab1eb3bdfe91790a
qfcy <3416445406@qq.com>
2022-07-14 12:50:07 +0800

    Update 二分法.py

--- 算法/二分法.py
+++ 算法/二分法.py
@@ -2,27 +2,27 @@ import math
 
 def getvalue(func,y,min,max):
     x = (min+max) / 2
-    flag1=0.4;flag2=0.6
+    flag1=0.4;flag2=0.6 # 用于判断递增或递减
     while True:
         val = func(x)
         if func(min*flag1+max*flag2) > val:
             # 递增
-            if val < y:
-                min = x
-            elif val > y:
-                max = x
-            elif math.isclose(y,val):
+            if math.isclose(y,val): # 对浮点数不用 == 比较
                 return x
+            elif val < y:
+                min = x
+            else:
+                max = x
         elif func(min*flag1+max*flag2) < val:
             # 递减
-            if val < y:
-                max = x
-            elif val > y:
-                min = x
-            elif math.isclose(y,val):
+            if math.isclose(y,val):
                 return x
+            elif val < y:
+                max = x
+            else:
+                min = x
         else:
-            flag1 = 0.25 + flag1 / 2
+            flag1 = 0.25 + flag1 / 2 # 减小flag1, flag2的范围
             flag2 = 0.25 + flag2 / 2
         x = (min+max) / 2
         print(min,max)
