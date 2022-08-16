b4c5033cc39a6a4cc724780a6b243d0b7d2f51d5
qfcy <3076711200@qq.com>
2022-08-16 10:19:42 +0800

    Update 生成随机数.py

--- 算法/生成随机数.py
+++ 算法/生成随机数.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from time import perf_counter
-from pprint import pprint
+from random import random as random2
 
 seed = perf_counter()
 k=5.6;b=3.5
@@ -9,14 +9,18 @@ def random():
     global seed
     seed=seed*k+b
     if seed > 10**10:
-        seed=perf_counter()
-    return seed % 1
+        seed=perf_counter() # 加入时间的随机因素
+    return seed % i
 
 l=[]
-for i in range(500):
+for j in range(500):
     l.append(random())
-    k=random()*5.6
+    k=random()*5.6 # 变化k, 避免随机数重复
 
 #pprint(l)
 plt.hist(l,bins=len(l)//5)
+plt.show()
+
+l2=[random2() for i in range(500)]
+plt.hist(l2,bins=len(l2)//5)
 plt.show()
