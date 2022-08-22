60ce88a9c18e76365cc4aef556988b71e424629c
qfcy <3076711200@qq.com>
2022-08-22 13:11:17 +0800

    Update 讲述人.py

--- 音频/讲述人.py
+++ 音频/讲述人.py
@@ -3,5 +3,9 @@ clr.AddReference("System.Speech")
 from System.Speech.Synthesis import *
 
 speak = SpeechSynthesizer()
-speak.SpeakAsync("Hello world")
+speak.SelectVoice('Microsoft Zira Desktop')
+#speak.SetOutputToWaveFile("输出.wav")
+speak.Speak("Hello world")
+speak.SelectVoice('Microsoft Huihui Desktop')
 speak.Speak("中文")
+#speak.SetOutputToDefaultAudioDevice()
