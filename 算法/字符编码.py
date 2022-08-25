a7af97efe5110b754f568d7d29b328aa09dfa557
qfcy <3076711200@qq.com>
2022-08-25 23:01:12 +0800

    Update 字符编码.py

--- 算法/字符编码.py
+++ 算法/字符编码.py
@@ -1,7 +1,7 @@
 "将字符转换为字符编码。"
 while True:
     n=input("输入字符:")
-    if len(n)==1:print(ord(n))
+    if len(n)==1:print(hex(ord(n)))
     else:
         for i in range(len(n)):
             print(hex(ord(n[i]))+"\t",end='')#逐个显示字符的编码
