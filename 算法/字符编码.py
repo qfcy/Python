b823c8cdf6acc41de124ba977f18a1e93723749f
qfcy <3076711200@qq.com>
2022-08-25 22:52:14 +0800

    Update 字符编、解码

--- 算法/字符编码.py
+++ 算法/字符编码.py
@@ -4,5 +4,5 @@ while True:
     if len(n)==1:print(ord(n))
     else:
         for i in range(len(n)):
-            print(str(ord(n[i]))+"\t",end='')#逐个显示字符的编码
+            print(hex(ord(n[i]))+"\t",end='')#逐个显示字符的编码
         print()
--- 算法/字符解码.py
+++ 算法/字符解码.py
@@ -3,5 +3,6 @@ while True:
     n=str(input("输入字符的编码:"))
     l=n.split()
     for n in l:
-        print(chr(int(n)),end="")#逐个显示字符的编码
+        try:print(chr(eval("0x"+n)),end="")#逐个显示字符的编码
+        except Exception as err:print(type(err).__name__,':',err)
     print()
