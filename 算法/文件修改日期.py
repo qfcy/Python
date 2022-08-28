6a3b37ac9c3bd27cbfcd496b38a8c2955f5aa780
qfcy <3076711200@qq.com>
2022-08-28 12:11:06 +0800

    Update 文件修改日期.py

--- 算法/文件修改日期.py
+++ 算法/文件修改日期.py
@@ -1,6 +1,6 @@
 import sys,os,time
 # 时间最高支持 2108 01 01 (07:59:58)
-format = "%Y %m %d"
+format = "%Y %m %d %H %M"
 time_ = input('输入 %s 格式的时间: '%format)
 for arg in sys.argv[1:]:
     os.utime(arg,
