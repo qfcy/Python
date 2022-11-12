821bb36caf5bb5f275b610c2ffd96d1019dd47a9
qfcy <3076711200@qq.com>
2022-11-13 04:05:42 +0800

    Update 文件修改日期

---
+++ 小项目/文件处理/批量更改文件修改日期.py
@@ -0,0 +1,8 @@
+import os,time,search_file
+
+for p in search_file.directories("E:\\python",dirs=False):
+    t=os.stat(p).st_ctime
+    tm=time.localtime(t)
+    if (tm.tm_hour >=22 or tm.tm_hour <=3):
+        print(p,tm.tm_mday,tm.tm_hour,tm.tm_min)
+        os.utime(p,times=(t+3600*9,)*2)
--- 小项目/文件处理/文件修改日期.py
+++ 小项目/文件处理/文件修改日期.py
@@ -1,5 +1,5 @@
 import sys,os,time
-# 时间最高支持 2108 01 01 (07:59:58)
+# 系统能显示的最大时间: 2108 01 01 (07:59:58)
 format = "%Y %m %d %H %M"
 time_ = input('输入 %s 格式的时间: '%format)
 for arg in sys.argv[1:]:
